5aeca035f12d201aa231a8d64f8d7b5115a61f62
Samugcor <samueligcorp@gmail.com>
2025-02-06 11:19:51 +0100

    Initial commit

---
+++ src/main/java/com/accesodatos/springboot_joboffers/SpringBootJobOffersApplication.java
@@ -0,0 +1,13 @@
+package com.accesodatos.springboot_joboffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJobOffersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJobOffersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accesodatos/springboot_joboffers/SpringBootJobOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accesodatos.springboot_joboffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJobOffersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
